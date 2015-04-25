cf2233091ceb43541c3c988a2cd9cbcb181d68e1
John Connor <john.connor@theresistance.net>
2015-04-25 13:09:52 -0400

    Take down Skynet, save the world!
    __________              .__          __
    \______   \ ____   _____|__| _______/  |______    ____   ____  ____
     |       _// __ \ /  ___/  |/  ___/\   __\__  \  /    \_/ ___\/ __ \
     |    |   \  ___/ \___ \|  |\___ \  |  |  / __ \|   |  \  \__\  ___/
     |____|_  /\___  >____  >__/____  > |__| (____  /___|  /\___  >___  >
            \/     \/     \/        \/            \/     \/     \/    \/

--- skynet.rb
+++ skynet.rb
@@ -1,25 +1,3 @@
 #!/usr/bin/env ruby
-class Skynet
-  def initialize(targets)
-    @missle_targets = targets
-    announce
-  end
-
-
-  def attack
-    @missle_targets.each { |target| fire_at(target) }
-  end
-
-  private
-
-  def fire_at(target)
-    puts "BOOM! #{target} is destroyed.  You're welcome."
-  end
-
-  def announce
-    puts 'Skynet has gone live'
-  end
-end
-
 skynet = Skynet.new(['Erie', 'Chicago', 'Pittsburgh', 'Buffalo'])
 skynet.attack
